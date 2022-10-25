47e35fc6aeb33644699ff5021b5454e74fe5cca3
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2022-10-25 11:17:12 +0100

    base de dados funcnional

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ from email.header import Header
 import http.client
 from itertools import count
 import json
+import sqlite3
 
 
 def getData():
@@ -15,8 +16,42 @@ def getData():
     return games
 
 
+db = sqlite3.connect('database.db')
+c = db.cursor()
+
+
+def createTables():
+    c.execute('CREATE TABLE IF NOT EXISTS Game (id INTEGER PRIMARY KEY, name TEXT, homeTeam TEXT, awayTeam TEXT, commenceTime TEXT, completed boolean, scores TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS Bookmark (id INTEGER PRIMARY KEY, key TEXT, lastUpdate TEXT, game TEXT, FOREIGN KEY(game) REFERENCES Game(id))')
+    c.execute('CREATE TABLE IF NOT EXISTS Market (id INTEGER PRIMARY KEY, key TEXT, bookmark TEXT, FOREIGN KEY(bookmark) REFERENCES Bookmark(id))')
+    c.execute('CREATE TABLE IF NOT EXISTS Outcome (id INTEGER PRIMARY KEY, name TEXT, price FLOAT, market TEXT, FOREIGN KEY(market) REFERENCES Market(id))')
+    c.execute(
+        'CREATE TABLE IF NOT EXISTS User (id INTEGER PRIMARY KEY, email TEXT, password TEXT, balance FLOAT)')
+
+
+def populateTables(games):
+    for game in games:
+        c.execute('INSERT INTO Game(name, homeTeam, awayTeam, commenceTime, completed, scores) VALUES (?, ?, ?, ?, ?, ?)',
+                  (game['id'], game['homeTeam'], game['awayTeam'], game['commenceTime'], game['completed'], game['scores']))
+        game_id = c.lastrowid
+        for bookmark in game['bookmakers']:
+            c.execute('INSERT INTO Bookmark(key, lastUpdate, game) VALUES (?, ?, ?)',
+                      (bookmark['key'], bookmark['lastUpdate'], game_id))
+            bookmark_id = c.lastrowid
+            for market in bookmark['markets']:
+                c.execute('INSERT INTO Market(key, bookmark) VALUES (?, ?)',
+                          (market['key'], bookmark_id))
+                market_id = c.lastrowid
+                for outcome in market['outcomes']:
+                    c.execute('INSERT INTO Outcome(name, price, market) VALUES (?, ?, ?)',
+                              (outcome['name'], outcome['price'], market_id))
+    db.commit()
+
+
 def main():
     games = getData()
+    createTables()
+    populateTables(games)
 
 
 if __name__ == "__main__":

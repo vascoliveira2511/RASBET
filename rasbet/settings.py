860e9ec883d7463f4f94490b7912478c401963c5
Berteotti <berteotti2@gmail.com>
2022-11-09 19:59:44 +0000

    feature: add models and update settings

--- rasbet/settings.py
+++ rasbet/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import dj_database_url
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rasbet'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +79,7 @@ WSGI_APPLICATION = 'rasbet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=env('DATABASE_URL'))}
 
 
 # Password validation

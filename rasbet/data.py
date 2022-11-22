ceb9c740d028fca61e588048a180ec23581c560d
Vasco Mota de Oliveira <vascoliveira2511@gmail.com>
2022-11-22 11:50:54 +0000

    Update data.py

--- rasbet/data.py
+++ rasbet/data.py
@@ -1,7 +1,10 @@
 import http
 import json
 from django.db import models
-from rasbet.models.game import *
+from rasbet.models.bookmaker import Bookmaker
+from rasbet.models.game import Game
+from rasbet.models.game import Market
+from rasbet.models.game import Outcome
 
 
 def getData():  # Get data from API
@@ -22,3 +25,15 @@ def updateData(games):  # Update data
         game = Game(game["name"], game["home_team"], game["away_team"],
                     game["commance_time"], game["completed"], game["scores"])
         game.save()  # Save data
+        for bookmaker in game["bookmakers"]:
+            bookmaker = Bookmaker(
+                bookmaker["key"], bookmaker["last_update"], game.id)
+            bookmaker.save()
+            for market in bookmaker["markets"]:
+                market = Market(
+                    market["key"], bookmaker.id)
+                market.save()
+                for outcome in market["outcomes"]:
+                    outcome = Outcome(
+                        outcome["name"], outcome["price"], market.id, bookmaker.id)
+                    outcome.save()
